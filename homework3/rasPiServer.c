37949edfc431cfe0108fbc42f6afa63dc297aebe
tsukolsky <sukolsky@bu.edu>
2013-03-19 19:33:13 -0400

    Everything compiles and soft tests work. Now going to test on RasPI

--- homework3/rasPiServer.c
+++ homework3/rasPiServer.c
@@ -11,7 +11,10 @@
 |
 |--------------------------------------------------------------------------------
 | Revisions: 3/18-Initial Build
-|
+|	     3/19-Tweaked this to go into a function on positive listen and then fork and 
+|		  exec. Initially had an issue with open pipes/blocking, fixed it though by
+|		  closing every single pipe not being used in the master parent. See commits
+|		  from earlier today. (2)Got the exec calls working with correct piping.
 |================================================================================
 | *NOTES:(1) Basic socket info was provided by the instructor. Other information on 
 |		  	 sockets can be found at ...
@@ -25,49 +28,53 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdbool.h>
+
+#define ONE_MB 1024
+#define NUMBER_OF_MB 5
 
 /*===============================*/
 /*	Forward Declarations	 */
 /*===============================*/
 void error(const char *msg);
-
+bool dealWithConnection(int socketHandle);
 	
 /*===============================*/
 /*      Main Program		 */
 /*===============================*/
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
 	//Declare Variables
-     int sockfd, newsockfd, portno;		//Server socket descriptor, client<->server socket descriptor, portno
-     socklen_t clilen;					//length of socket name for client
-     char buffer[256];					//buffer length we can write to
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;								//length used for certain things
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-	 //Create server socket
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
+        int sockfd, newsockfd, portno;		//Server socket descriptor, client<->server socket descriptor, portno
+        socklen_t clilen;					//length of socket name for client
+        char buffer[256];					//buffer length we can write to
+        struct sockaddr_in serv_addr, cli_addr;
+        int n;								//length used for certain things
+        if (argc < 2) {
+                fprintf(stderr,"ERROR, no port provided\n");
+        	exit(1);
+        }
+
+	//Create server socket
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd < 0){error("ERROR opening socket");}
+
+        bzero((char *) &serv_addr, sizeof(serv_addr));
+        portno = atoi(argv[1]);
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_addr.s_addr = INADDR_ANY;
+        serv_addr.sin_port = htons(portno);
 	 
-	 //Bind local address to server socket
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
+        //Bind local address to server socket
+        if (bind(sockfd, (struct sockaddr *) &serv_addr,
+     	sizeof(serv_addr)) < 0) 
+        error("ERROR on binding");
 	
-	 //Now listen
-     listen(sockfd,5);
+        //Now listen
+  	listen(sockfd,5);
 	 
 	 //THis is what should continue and contine and continue
-	 for (;;){
+	for (;;){
 		clilen = sizeof(cli_addr);
 		newsockfd = accept(sockfd, 
                  (struct sockaddr *) &cli_addr, 
@@ -78,9 +85,13 @@ int main(int argc, char *argv[])
 		//Print out something
 		n=write(newsockfd,"I am connected and forking to you.",40);
 		if (n<0){error("ERROR writing to socket...");}	
-		else {printf("I sent what was requested.\n");}
+		else {
+			bool successful = dealWithConnection(newsockfd);
+			if (successful){printf("I send the correct information.\r\n");}
+			else{printf("Resolving error...\r\n");}
+		}//end else
 		close(newsockfd);
-	 }
+	 }//end infinite for
      close(sockfd);
      return 0; 
 }
@@ -92,56 +103,63 @@ int main(int argc, char *argv[])
 void error(const char *msg)
 {
     perror(msg);
-    exit(1);
+  // exit(1);
 }
 
-void dealWithConnection(int socketHandle){
+bool dealWithConnection(int socketHandle){
+	const int bufferSize=ONE_MB*NUMBER_OF_MB;
 	int pid1,pid2,dmesgPipe[2],tailPipe[2],n,n2;
-	char buffer1[BUFFER_SIZE];
-	char *tailCmd[] = {"tail","-l", "50", 0};
+	char buffer1[bufferSize];
+	char *tailCmd[] = {"tail","-n", "30", 0};
 	char *dmesgCmd[] = {"dmesg",0};
-	
-	if (pipe(dmesgPipe) <0){error("Error creating pipe 1.");}
-	if (pipe(tailPipe) < 0){error("Error creating tail pipe.";}
 
-	if ((pid1=fork())==0){//Child process
-		//We are going to run dmesg and pipe that into tail -30.
-		if ((pid2=fork())==0){	//child process (tail)
+	//Make the pipes
+	if (pipe(dmesgPipe) < 0){error("Unable to create dmesg pipe.");return false;}
+	if (pipe(tailPipe) < 0){error("Unable to create tail pipe."); return false;}
+
+	//Fork and execute "dmesg | tail -n 30"
+	if ((pid1=fork())==0){
+		if ((pid2=fork())==0){	//this is the dmesg program
+			//Assign standard output to the output side of th pipe. Close the input side.
 			dup2(dmesgPipe[1],1);
-			dup2(dmesgPipe[1],2);
+			close(dmesgPipe[0]);
+			close(2);
+				
+			//Execute the program "dmesg"
+			execvp(dmesgCmd[0],dmesgCmd);
+		}else if (pid2>0){	//This is the tail process, parent of dmesg and child of the server
+			//Assign standard input of this pipe as the input end of the dmesgPipe (coming from dmesg). 			
+			dup2(dmesgPipe[0],0);	
+			close(dmesgPipe[1]);		//Close output end
+			close(2);
 			
-			close(dmesgPipe[0]);		//close input of the dmesg pipe in parent process
-							//Output goes to tail command which is parent process
-			execvp(dmesgCmd[0],dmesgCmd);	//executes dmesg command and is sent to tail(parent process.)
+			//Assign standard output to go to output end of the tailPipe. 
+			dup2(tailPipe[1],1);
+			close(tailPipe[0]);		//close the input of this pipe
 
-		} else if (pid2 <0){error("Error creating tail process.");}
-		else {	//tail parent process, this is dmesg
-			//Duplicate an input stream from the dmesgPipe
-			dup2(dmesgPipe[0],0);
-			close(dmesgPipe[1]);		//close otput end of the pipe
-			
-			dup2(tailPipe[1],1);		//clone stdout and stderr for the tail
-			dup2(tailPipe[1],2);
-			close(tailPipe[0]);		//close input end, only writing to the parent process
-			
-			//Execute "tail -l 30" command, output goes to the parent process
+			//Exec the program, wait afterwards for the dmesg program to finish.
 			execvp(tailCmd[0],tailCmd);
+			waitpid(pid2,NULL,0);
+		}else {error("Error forking/creating \"dmesg\" program"); return false;}
 
-			//Wait for child process
-			waitpid(pid2,NULL,0);		//no zombies
-		}	//end parent process (dmesg)
-		
-	} else if (pid1 < 0){error("Error creating tail and dmesg process.");}
-	} else {
-		//This is the real output. We are going to be printing this stuff to the client
-		dup2(tailPipe[0],0);		//stdin is coming from the tailPipe
-		close(tailPipe[1]);		//close output
+	}else if (pid1>0){	//parent of "tail" program, grandparent of "dmesg" program
 
-		n = read(tailPipe[0],buffer1,BUFFER_SIZE);	//read into a buffer that can then be printed back to the socket
-		n2 = write(sockedHandle,buffer1,sizeof(buffer1));	//print to the socket.
+		//Close all dmesgPipes to stop blocking, close output end of tailPIpe before trying to read. 
+		close(dmesgPipe[0]);
+		close(dmesgPipe[1]);
+		close(tailPipe[1]);
+
+		//Read from pipe, then forward to socket/client
+		n=read(tailPipe[0],buffer1,bufferSize);				//read
+		if (n<0){error("Error receiving data from pipes.");return false;}
+		n2 = write(socketHandle,buffer1,sizeof(buffer1));		//Print to socket
+		if (n2<0){error("Error writing data to socket.");return false;}		
 		
-		waitpid(pid1,NULL,0);		//no zombie processes please.
-	}		
+		//Wait for child process to finish		
+		waitpid(pid1,NULL,0);
+		return true;
+	}else {error("Error forking/creating \"tail\" program."); return false;
+}	
 }
 
 
--- parallelDirectory/homework3/easierTest.c
+++ parallelDirectory/homework3/easierTest.c
@@ -8,6 +8,9 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define ONE_MB 1024
+#define NUMBER_MB 5
+
 void function(void);
 
 int main(){
@@ -16,18 +19,19 @@ int main(){
 }
 
 void function(){
+	const int bufferSize=ONE_MB*NUMBER_MB;
 	char *dmesgCmd[] = {"dmesg",0};
 	char *tailCmd[] = {"tail","-n","30",0};
-	char buffer[1024];
+	char buffer[bufferSize];
 
 	int pipe1[2],pipe2[2];
 	pipe(pipe1);
-//	pipe(pipe2);
+	pipe(pipe2);
 	
 	int pid = fork();
 	if (pid==0){
 		int pid2= fork();
-		if (pid2==0){
+		if (pid2==0){	//this is the dmesg program
 			dup2(pipe1[1],1);
 			close(pipe1[0]);
 			close(2);
@@ -37,17 +41,19 @@ void function(){
 			dup2(pipe1[0],0);
 			close(pipe1[1]);
 			close(2);
+			
+			dup2(pipe2[1],1);
+			close(pipe2[0]);	//close the input of this pipe
 
-			int n=read(pipe1[0],buffer,1024);
 			execvp(tailCmd[0],tailCmd);
 			waitpid(pid2,NULL,0);
 
 		}else;	
 	}else if (pid>0){
-//Works after this
-
 		close(pipe1[0]);
 		close(pipe1[1]);
+		close(pipe2[1]);
+		int n=read(pipe2[0],buffer,bufferSize);
 		printf("%s", buffer);
 		waitpid(pid,NULL,0);
 	}else;
